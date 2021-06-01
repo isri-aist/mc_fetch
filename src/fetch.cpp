92453843bbee4820b6b8cf60f0f94cc3e3274bbf
Iori Kumagai <iori-kumagai@aist.go.jp>
2021-06-01 19:32:43 +0900

    [mc_fetch] add comment

--- src/fetch.cpp
+++ src/fetch.cpp
@@ -51,6 +51,7 @@ namespace mc_robots
     };
 
     _minimalSelfCollisions = {
+      // TODO: fingers cannot be included because they does not have dae mesh file in fetch_description
       mc_rbdyn::Collision("base_link", "elbow_flex_link", 0.01, 0.001, 0.),
       mc_rbdyn::Collision("base_link", "forearm_roll_link", 0.01, 0.001, 0.),
       mc_rbdyn::Collision("base_link", "wrist_flex_link", 0.01, 0.001, 0.),
